eafb5a6fbbc19e14ee971727fd1f9f3c553a2a41
Alhely CC <alhelycerveracastillo01@gmail.com>
2024-04-08 13:18:33 -0600

    add: modificacion de texto

--- frontend/views/alumnos/index.php
+++ frontend/views/alumnos/index.php
@@ -10,7 +10,7 @@ use yii\grid\GridView;
 /** @var app\models\AlumnosSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Alumnos';
+$this->title = 'Crear Alumnos';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="alumnos-index">
